c2736b0ccc19e77e476ea99fb72a63fa653208de
Nenad Rogić <nrogic@paycor.com>
2021-10-18 14:16:21 +0200

    again: 2 re-renders

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,9 @@ import Demo from './Demo';
 
 const App = () => {
 	return (
-		<section>
-			<div className="my-6 px-6">
-				<Demo />
-			</div>
-		</section>
+		<div className="my-6 px-6">
+			<Demo />
+		</div>
 	);
 };
 
