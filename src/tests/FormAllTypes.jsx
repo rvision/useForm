dbca180ad21a42b2f8355dc19c8d4756c6bca49c
Nenad Rogić <nrogic@paycor.com>
2021-11-10 18:12:54 +0100

    tests wip

---
+++ src/tests/FormAllTypes.jsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import useForm, { yupResolver } from '../useForm';
+
+const FormAllTypes = ({ schema,model, onSubmit}) => {
+  const { register, handleSubmit } = useForm({
+    defaultValues: model,
+	resolver: yupResolver(schema)
+
+  });
+
+  return (
+    <div>
+      <label>
+        Enter first name:
+        <input type="text" {...register("firstName")} />
+      </label>
+      <label>
+        Enter last name:
+        <input type="text" {...register("lastName")} />
+      </label>
+      <label>
+        Enter email:
+        <input type="email" {...register("email")} />
+      </label>
+      <label>
+        <input type="checkbox" {...register("agree")} />I agree to terms and
+        conditions
+      </label>
+      <button type="submit" onClick={handleSubmit(onSubmit)}>
+        Register
+      </button>
+    </div>
+  );
+};
+
+export default FormAllTypes;
